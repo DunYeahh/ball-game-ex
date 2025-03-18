363107e40e22164470f06a470a1336a9682df2c3
Dana Ziv <dana@ziv.co>
2025-03-18 16:17:48 +0200

    Add onBallClick function

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-function onBallClick(){
-    console.log('hi')
+function onBallClick(elBall){
+    const height = elBall.offsetHeight
+    const width = elBall.offsetWidth
+    elBall.style.height = `${height+50}px`
+    elBall.style.width = `${width+50}px`   
+    elBall.innerHTML = `${width+50}` 
 }
