4034579098337407e5432de47cb01e2c1ced2284
Dana Ziv <dana@ziv.co>
2025-03-18 17:34:03 +0200

    Edit ballClick function

--- main.js
+++ main.js
@@ -2,11 +2,17 @@ function onBallClick(elBall){
     var height = elBall.offsetHeight
     var width = elBall.offsetWidth
     if (height >= 400){
-        height = 50
-        width = 50
+        height = 100
+        width = 100
+        elBall.innerHTML = `${100}` 
+        elBall.style.height = `${height}px`
+        
+        elBall.style.width = `${width}px`
+        return   
     }
-    elBall.style.height = `${height+50}px`
+    var sizeAdd = getRandomInt(20, 61)
+    elBall.style.height = `${height+sizeAdd}px`
     
-    elBall.style.width = `${width+50}px`   
-    elBall.innerHTML = `${width+50}` 
+    elBall.style.width = `${width+sizeAdd}px`   
+    elBall.innerHTML = `${width+sizeAdd}` 
 }
