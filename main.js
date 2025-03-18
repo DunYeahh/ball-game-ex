6b9afab73659c358062ddbef2e7ad96e2c0af8c3
Dana Ziv <dana@ziv.co>
2025-03-18 17:55:53 +0200

    Add parameter to function

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-function onBallClick(elBall){
+function onBallClick(elBall, maxDiameter){
     elBall.style.backgroundColor = `${getRandomColor()}`
     var height = elBall.offsetHeight
     var width = elBall.offsetWidth
-    if (height >= 400){
+    if (height >= maxDiameter){
         height = 100
         width = 100
         elBall.innerHTML = `${100}` 
