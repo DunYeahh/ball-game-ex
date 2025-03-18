4c7fdcff6c20ca31be3a8a26a6b5ecb0a7f9ac1f
Dana Ziv <dana@ziv.co>
2025-03-18 17:01:47 +0200

    Edit Ball function

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 function onBallClick(elBall){
-    const height = elBall.offsetHeight
-    const width = elBall.offsetWidth
+    var height = elBall.offsetHeight
+    var width = elBall.offsetWidth
+    if (height >= 400){
+        height = 50
+        width = 50
+    }
     elBall.style.height = `${height+50}px`
+    
     elBall.style.width = `${width+50}px`   
     elBall.innerHTML = `${width+50}` 
 }
